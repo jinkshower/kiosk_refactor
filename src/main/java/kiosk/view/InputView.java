dc38936e2fcfb30b74301829ad8474ef4e297db9
jinkshower <askthem@naver.com>
2024-01-05 12:48:18 +0900

    feat(InputView): implement read main command function

--- src/main/java/kiosk/view/InputView.java
+++ src/main/java/kiosk/view/InputView.java
@@ -1,4 +1,17 @@
 package kiosk.view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public int readMainCommand() {
+        String input = scanner.nextLine().trim();
+        try {
+            return Integer.parseInt(input);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException(String.format("[ERROR]숫자만 입력할 수 있습니다." + input));
+        }
+    }
 }
